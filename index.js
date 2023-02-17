67340bfff3aa1db68040cb129a542ed7de726965
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-02-17 01:13:25 -0500

    Bug: CORS enabled

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+var cors = require("cors");
 
-const companyRoutes = require('./routes/companyRoute');
-const interviewRoutes = require('./routes/interviewRoute');
+const companyRoutes = require("./routes/companyRoute");
+const interviewRoutes = require("./routes/interviewRoute");
 
-const app = express()
-mongoose.set('strictQuery', false)
+const app = express();
+mongoose.set("strictQuery", false);
 
-const username = "admin"
-const password = "welcome%40123"
-const cluster = "windsor-cluster"
+const username = "admin";
+const password = "welcome%40123";
+const cluster = "windsor-cluster";
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.use(cors());
 
-app.use('/api', companyRoutes)
-app.use('/api', interviewRoutes)
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/api", companyRoutes);
+app.use("/api", interviewRoutes);
 
 mongoose.connect(
-  `mongodb+srv://${username}:${[password]}@windsor-cluster.qufpjwv.mongodb.net/lancer_view_db?retryWrites=true&w=majority`, 
+  `mongodb+srv://${username}:${[
+    password,
+  ]}@windsor-cluster.qufpjwv.mongodb.net/lancer_view_db?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   }
 );
 
@@ -34,8 +39,7 @@ db.on("error", console.error.bind(console, "connection error: "));
 
 db.once("open", function () {
   console.log("MongoDB connected successfully");
-  app.listen(3000, ()=> {
-    console.log("Listening on port 3000");
-  })
+  app.listen(3000, () => {
+    console.log("Listening on port 5000");
+  });
 });
-
