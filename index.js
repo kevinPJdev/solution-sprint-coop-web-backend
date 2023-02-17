95e4bcf9bfaff9d718cd429c7f4105e7d68ada08
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-02-17 01:15:07 -0500

    Bug: CORS enabled

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("MongoDB connected successfully");
   app.listen(3000, () => {
-    console.log("Listening on port 5000");
+    console.log("Listening on port 3000");
   });
 });
